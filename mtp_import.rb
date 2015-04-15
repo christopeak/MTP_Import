56b02526db9a4df4cbaf45bfa83cd96e1237491b
Chris Peak <cpeak@psrc.org>
2015-04-15 16:38:20 -0700

    first try at implementing variable seeting from hash

--- mtp_import.rb
+++ mtp_import.rb
@@ -1,10 +1,18 @@
 
 require_relative 'Project'
+require 'active_support'
+
+class String
+  def is_integer?
+    self.to_i.to_s == self
+  end
+end 
 
 class PlanProject < Project
   require 'nokogiri'
   attr :mtpid, :secondary_improvement_types
-  
+
+
   def nodeset_content(nodeset)
     a = []
     nodeset.each {|e| a<< e.content}
@@ -13,90 +21,82 @@ class PlanProject < Project
 
   def initialize(node)
     self.set_vars
-    c = node.children
-    @mtpid = c.css('project-id').first.content.to_i
-    @title = c.css('title').first.content
-    @title.gsub!("'",'`')
-    @title.gsub!('"','`')
-    @description = c.css('description').first.content
-    @description.gsub!("'",'`')
-    @description.gsub!('"','`')
-    @tot_proj_cost = c.css('total-cost').first.content.to_i
-    contact_first_name = c.css('contact-first-name').first.content
-    contact_last_name = c.css('contact-last-name').first.content
-    @contact_name = contact_first_name + ' ' + contact_last_name
-    @contact_phone = c.css('contact-phone').first.content
-    @contact_email = c.css('contact-email').first.content
-    @est_cost_year = c.css('constant-dollar-year').first.content.to_i
-    @completion_year = c.css('completion-year').first.content.to_i
-    @mtp_status = c.css('mtp-status').first.content
-    @project_on = c.css('location').first.content
-    @project_on.gsub!('"','')
-    @project_from = c.css('endpoint-a').first.content
-    @project_to = c.css('endpoint-b').first.content
-    @mile_post_from = c.css('milepost-a').first.content.empty? ? 'NULL' : c.css('milepost-a').first.content
-    @mile_post_to = c.css('milepost-b').first.content.empty? ? 'NULL' : c.css('milepost-b').first.content
-    @county_id = c.css('county-id').first.content.empty? ? 'NULL' : c.css('county-id').first.content
-    @func_class_id = c.css('functional-class').first.content.empty? ? 'NULL' : c.css('functional-class').first.content
+    @vars.each do |k, v|
+      c = node.children
+      case v[1]
+      when 'string'
+        value = c.css(v[0]).first.content
+        value.gsub! "'", "`"
+        instance_variable_set("@#{k}", value)
+      when 'int', 'decimal'
+        value = c.css(v[0]).first.content.empty? ? 'NULL' : c.css(v[0]).first.content.to_f
+        instance_variable_set("@#{k}", value)
+      when 'bit'
+        value = c.css(v[0]).first.content.first.content == 'true' ? 1 : 0
+        instance_variable_set("@#{k}", value)      
+      end
+    end
+    @contact_name = @contact_first_name + ' ' + @contact_last_name
     @start_year = c.css('start-year').first.content[0,4]
-    @primary_improvement_type_id = c.css('primary-improvement-type-id').first.content
+
     s_i_t = c.css('secondary-improvement-types').children 
     @secondary_improvement_types = nodeset_content(s_i_t.css('number').children)
-    @p_a1a = c.css('prioritization-a1a').first.content == 'true' ? 1 : 0
-    @p_a1b = c.css("prioritization-a1b").first.content == 'true' ? 1 : 0
-    @p_a2a = c.css("prioritization-a2a").first.content== 'true'  ? 1 : 0
-    @p_a2b = c.css("prioritization-a2b").first.content == 'true'  ? 1 : 0
-    @p_a3 = c.css("prioritization-a3").first.content == 'true' ? 1 : 0
-    @p_a4 = c.css("prioritization-a4").first.content == 'true' ? 1 : 0
-    @p_c1a = c.css("prioritization-c1a").first.content == 'true' ? 1 : 0
-    @p_c1b = c.css("prioritization-c1b").first.content == 'true' ? 1 : 0
-    @p_c1c = c.css("prioritization-c1c").first.content == 'true' ? 1 : 0
-    @p_c2a = c.css("prioritization-c2a").first.content == 'true' ? 1 : 0
-    @p_c2b = c.css("prioritization-c2b").first.content == 'true' ? 1 : 0
-    @p_c3 = c.css("prioritization-c3").first.content == 'true' ? 1 : 0
-    @p_c4 = c.css("prioritization-c4").first.content == 'true' ? 1 : 0
-    @p_c5 = c.css("prioritization-c5").first.content == 'true' ? 1 : 0
-    @p_f1 = c.css("prioritization-f1").first.content == 'true' ? 1 : 0
-    @p_f2 = c.css("prioritization-f2").first.content == 'true' ? 1 : 0
-    @p_f3 = c.css("prioritization-f3").first.content == 'true' ? 1 : 0
-    @p_f4a = c.css("prioritization-f4a").first.content == 'true' ? 1 : 0
-    @p_f4b = c.css("prioritization-f4b").first.content == 'true' ? 1 : 0
-    @p_f5 = c.css("prioritization-f5").first.content == 'true' ? 1 : 0
-    @p_f6 = c.css("prioritization-f6").first.content == 'true' ? 1 : 0
-    @p_j1a = c.css("prioritization-j1a").first.content == 'true' ? 1 : 0
-    @p_j1b = c.css("prioritization-j1b").first.content == 'true' ? 1 : 0
-    @p_j2 = c.css("prioritization-j2").first.content == 'true' ? 1 : 0
-    @p_j3 = c.css("prioritization-j3").first.content == 'true' ? 1 : 0
-    @p_j4 = c.css("prioritization-j4").first.content == 'true' ? 1 : 0
-    @p_m1 = c.css("prioritization-m1").first.content == 'true' ? 1 : 0
-    @p_m2 = c.css("prioritization-m2").first.content == 'true' ? 1 : 0
-    @p_m3 = c.css("prioritization-m3").first.content == 'true' ? 1 : 0
-    @p_m4 = c.css("prioritization-m4").first.content == 'true' ? 1 : 0
-    @p_m5 = c.css("prioritization-m5").first.content == 'true' ? 1 : 0
-    @p_m6 = c.css("prioritization-m6").first.content == 'true' ? 1 : 0
-    @p_m7 = c.css("prioritization-m7").first.content == 'true' ? 1 : 0
-    @p_o1 = c.css("prioritization-o1").first.content == 'true' ? 1 : 0
-    @p_o2a = c.css("prioritization-o2a").first.content == 'true' ? 1 : 0
-    @p_o2b = c.css("prioritization-o2b").first.content == 'true' ? 1 : 0
-    @p_o2c = c.css("prioritization-o2c").first.content == 'true' ? 1 : 0
-    @p_o3a = c.css("prioritization-o3a").first.content == 'true' ? 1 : 0
-    @p_o3b = c.css("prioritization-o3b").first.content == 'true' ? 1 : 0
-    @p_o3c = c.css("prioritization-o3c").first.content == 'true' ? 1 : 0
-    @p_s1a = c.css("prioritization-s1a").first.content == 'true' ? 1 : 0
-    @p_s1b = c.css("prioritization-s1b").first.content == 'true' ? 1 : 0
-    @p_s1c = c.css("prioritization-s1c").first.content == 'true' ? 1 : 0
-    @p_s2 = c.css("prioritization-s2").first.content == 'true' ? 1 : 0
-    @p_t1 = c.css("prioritization-t1").first.content == 'true' ? 1 : 0
-    @p_t2 = c.css("prioritization-t2").first.content == 'true' ? 1 : 0
-    @p_t3 = c.css("prioritization-t3").first.content == 'true' ? 1 : 0
-    @p_t4 = c.css("prioritization-t4").first.content == 'true' ? 1 : 0
-    @p_w1a = c.css("prioritization-w1a").first.content == 'true' ? 1 : 0
-    @p_w1b = c.css("prioritization-w1b").first.content == 'true' ? 1 : 0
-    @p_w1c = c.css("prioritization-w1c").first.content == 'true' ? 1 : 0
-    @p_w1d = c.css("prioritization-w1d").first.content == 'true' ? 1 : 0
-    @p_w2 = c.css("prioritization-w2").first.content == 'true' ? 1 : 0
-    @p_w4a = c.css("prioritization-w4a").first.content == 'true' ? 1 : 0
-    @p_w4b = c.css("prioritization-w4b").first.content == 'true' ? 1 : 0
+
+    # @p_a1a = c.css('prioritization-a1a').first.content == 'true' ? 1 : 0
+    # @p_a1b = c.css("prioritization-a1b").first.content == 'true' ? 1 : 0
+    # @p_a2a = c.css("prioritization-a2a").first.content== 'true'  ? 1 : 0
+    # @p_a2b = c.css("prioritization-a2b").first.content == 'true'  ? 1 : 0
+    # @p_a3 = c.css("prioritization-a3").first.content == 'true' ? 1 : 0
+    # @p_a4 = c.css("prioritization-a4").first.content == 'true' ? 1 : 0
+    # @p_c1a = c.css("prioritization-c1a").first.content == 'true' ? 1 : 0
+    # @p_c1b = c.css("prioritization-c1b").first.content == 'true' ? 1 : 0
+    # @p_c1c = c.css("prioritization-c1c").first.content == 'true' ? 1 : 0
+    # @p_c2a = c.css("prioritization-c2a").first.content == 'true' ? 1 : 0
+    # @p_c2b = c.css("prioritization-c2b").first.content == 'true' ? 1 : 0
+    # @p_c3 = c.css("prioritization-c3").first.content == 'true' ? 1 : 0
+    # @p_c4 = c.css("prioritization-c4").first.content == 'true' ? 1 : 0
+    # @p_c5 = c.css("prioritization-c5").first.content == 'true' ? 1 : 0
+    # @p_f1 = c.css("prioritization-f1").first.content == 'true' ? 1 : 0
+    # @p_f2 = c.css("prioritization-f2").first.content == 'true' ? 1 : 0
+    # @p_f3 = c.css("prioritization-f3").first.content == 'true' ? 1 : 0
+    # @p_f4a = c.css("prioritization-f4a").first.content == 'true' ? 1 : 0
+    # @p_f4b = c.css("prioritization-f4b").first.content == 'true' ? 1 : 0
+    # @p_f5 = c.css("prioritization-f5").first.content == 'true' ? 1 : 0
+    # @p_f6 = c.css("prioritization-f6").first.content == 'true' ? 1 : 0
+    # @p_j1a = c.css("prioritization-j1a").first.content == 'true' ? 1 : 0
+    # @p_j1b = c.css("prioritization-j1b").first.content == 'true' ? 1 : 0
+    # @p_j2 = c.css("prioritization-j2").first.content == 'true' ? 1 : 0
+    # @p_j3 = c.css("prioritization-j3").first.content == 'true' ? 1 : 0
+    # @p_j4 = c.css("prioritization-j4").first.content == 'true' ? 1 : 0
+    # @p_m1 = c.css("prioritization-m1").first.content == 'true' ? 1 : 0
+    # @p_m2 = c.css("prioritization-m2").first.content == 'true' ? 1 : 0
+    # @p_m3 = c.css("prioritization-m3").first.content == 'true' ? 1 : 0
+    # @p_m4 = c.css("prioritization-m4").first.content == 'true' ? 1 : 0
+    # @p_m5 = c.css("prioritization-m5").first.content == 'true' ? 1 : 0
+    # @p_m6 = c.css("prioritization-m6").first.content == 'true' ? 1 : 0
+    # @p_m7 = c.css("prioritization-m7").first.content == 'true' ? 1 : 0
+    # @p_o1 = c.css("prioritization-o1").first.content == 'true' ? 1 : 0
+    # @p_o2a = c.css("prioritization-o2a").first.content == 'true' ? 1 : 0
+    # @p_o2b = c.css("prioritization-o2b").first.content == 'true' ? 1 : 0
+    # @p_o2c = c.css("prioritization-o2c").first.content == 'true' ? 1 : 0
+    # @p_o3a = c.css("prioritization-o3a").first.content == 'true' ? 1 : 0
+    # @p_o3b = c.css("prioritization-o3b").first.content == 'true' ? 1 : 0
+    # @p_o3c = c.css("prioritization-o3c").first.content == 'true' ? 1 : 0
+    # @p_s1a = c.css("prioritization-s1a").first.content == 'true' ? 1 : 0
+    # @p_s1b = c.css("prioritization-s1b").first.content == 'true' ? 1 : 0
+    # @p_s1c = c.css("prioritization-s1c").first.content == 'true' ? 1 : 0
+    # @p_s2 = c.css("prioritization-s2").first.content == 'true' ? 1 : 0
+    # @p_t1 = c.css("prioritization-t1").first.content == 'true' ? 1 : 0
+    # @p_t2 = c.css("prioritization-t2").first.content == 'true' ? 1 : 0
+    # @p_t3 = c.css("prioritization-t3").first.content == 'true' ? 1 : 0
+    # @p_t4 = c.css("prioritization-t4").first.content == 'true' ? 1 : 0
+    # @p_w1a = c.css("prioritization-w1a").first.content == 'true' ? 1 : 0
+    # @p_w1b = c.css("prioritization-w1b").first.content == 'true' ? 1 : 0
+    # @p_w1c = c.css("prioritization-w1c").first.content == 'true' ? 1 : 0
+    # @p_w1d = c.css("prioritization-w1d").first.content == 'true' ? 1 : 0
+    # @p_w2 = c.css("prioritization-w2").first.content == 'true' ? 1 : 0
+    # @p_w4a = c.css("prioritization-w4a").first.content == 'true' ? 1 : 0
+    # @p_w4b = c.css("prioritization-w4b").first.content == 'true' ? 1 : 0
   end
   
   def import_qry
